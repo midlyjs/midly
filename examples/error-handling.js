b503931a531c090789304ae4f9731879c8d0781d
Vlad Afonin <afonin.com123@gmail.com>
2019-07-09 18:51:59 +0300

    Create error-handling.js

---
+++ examples/error-handling.js
@@ -0,0 +1,20 @@
+
+require('../lib/midly.js')()
+.use((req, res, ctx) => {
+	ctx.error('error1')
+	ctx.errors.push(new Error('custom'))
+	ctx.error('error2', 404)
+	res.body = 'Hello world!' // it will be replaced
+})
+.use((req, res, ctx) => {
+	ctx.error('error3')
+	throw ctx.error('error4')
+	// never run
+	ctx.error('error5')
+})
+.listen(3000)
+.on('error', (req, res, ctx) => {
+	console.log(ctx.errors, res.code)
+	res.body = 'Oh yes, I caught them all:\n' + ctx.errors.join(',\n')
+})
+console.log('Midly started on port 3000')
